4eedba1f55f8c22b2015a3cc9df6587ea1cca2dc
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-28 21:38:41 +0300

    Adding child subscription and implicit cancellation

--- src/code.ts
+++ src/code.ts
@@ -1,4 +1,6 @@
 import { Observable } from "rxjs/Observable";
+import { interval } from "rxjs/observable/interval";
+import { take } from "rxjs/operators";
 
 const observer = (obs: any) => {
 	obs.next('hi');
@@ -17,6 +19,17 @@ const subscribe = {
 	complete: () => console.log('Complete!')
 }
 observable.subscribe(subscribe);
+
+const obsWithInterval = interval(1000).pipe(take(5));
+const sub1 = obsWithInterval.subscribe((val) => addItem(`from obsInterval: ${val}`));
+
+const obsWithInterval2 = interval(1000).pipe(take(5));
+const sub2 = obsWithInterval2.subscribe((val) => addItem(`I will be cancelled: ${val}`));
+setTimeout(() => {
+	sub2.unsubscribe()
+}, 4000);
+
+sub2.add(sub1); // sub1 is cancelled along with sub2 which is cancelled explicitly
 
 function addItem(v: string) {
 	const node = document.createElement('li');
