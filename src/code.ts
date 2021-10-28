eeb0d45141073037b305e6edd8a10266048f244a
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-28 13:45:47 +0300

    The first `cold` observable

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,26 @@
-import * as Rx from "rxjs/Observable";
+import { Observable } from "rxjs/Observable";
 
-console.log(Rx);
+const observer = (obs: any) => {
+	obs.next('hi');
+	setTimeout(() => {
+		obs.next(`it's me`);
+		// obs.error('Oooops...');
+		obs.complete();
+	}, 1000);
+}
+
+const observable = Observable.create(observer);
+
+const subscribe = {
+	next: (v: string) => addItem(v),
+	error: (e: any) => console.error(e),
+	complete: () => console.log('Complete!')
+}
+observable.subscribe(subscribe);
+
+function addItem(v: string) {
+	const node = document.createElement('li');
+	const textNode = document.createTextNode(v);
+	node.appendChild(textNode);
+	document.getElementById('output').appendChild(node);
+}
