e90d2121ce38f01b8af9892c78fd36851d9a6cf0
Natalya Ivanova <natalyas.ivanova@gmail.com>
2021-10-28 21:55:39 +0300

    `Hot` Observable with mousemove event

--- src/code.ts
+++ src/code.ts
@@ -1,6 +1,7 @@
 import { Observable } from "rxjs/Observable";
+import { take, tap, pluck } from "rxjs/operators";
 import { interval } from "rxjs/observable/interval";
-import { take } from "rxjs/operators";
+import { fromEvent } from "rxjs/observable/fromEvent";
 
 const observer = (obs: any) => {
 	obs.next('hi');
@@ -11,25 +12,22 @@ const observer = (obs: any) => {
 	}, 1000);
 }
 
-const observable = Observable.create(observer);
+const observable = fromEvent(document, 'mousemove');
 
 const subscribe = {
 	next: (v: string) => addItem(v),
 	error: (e: any) => console.error(e),
 	complete: () => console.log('Complete!')
 }
-observable.subscribe(subscribe);
 
-const obsWithInterval = interval(1000).pipe(take(5));
-const sub1 = obsWithInterval.subscribe((val) => addItem(`from obsInterval: ${val}`));
-
-const obsWithInterval2 = interval(1000).pipe(take(5));
-const sub2 = obsWithInterval2.subscribe((val) => addItem(`I will be cancelled: ${val}`));
 setTimeout(() => {
-	sub2.unsubscribe()
-}, 4000);
-
-sub2.add(sub1); // sub1 is cancelled along with sub2 which is cancelled explicitly
+	observable
+	.pipe(
+		pluck('clientX'),
+		tap(e => console.log(e))
+	)
+	.subscribe(subscribe);
+}, 3000);
 
 function addItem(v: string) {
 	const node = document.createElement('li');
